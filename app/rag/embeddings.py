7949ecac85151d72292cb72065eff506cce61b15
phanikumar822 <1818phani@gmail.com>
2026-08-18 21:36:08 +0530

    Fix memory crash by moving embeddings to Hugging Face API

--- app/rag/embeddings.py
+++ app/rag/embeddings.py
@@ -1,45 +1,37 @@
-from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+from langchain_huggingface import HuggingFaceEndpointEmbeddings
 from langchain_qdrant import QdrantVectorStore
 from qdrant_client import QdrantClient
 from qdrant_client.models import VectorParams, Distance
-
 import os
-
 
 def get_qdrant_url():
     return os.getenv("QDRANT_URL", "http://localhost:6333")
-
 
 clients = QdrantClient(
     url=get_qdrant_url(),
     api_key=os.getenv("QDRANT_API_KEY", None)
 )
 
-if not clients.collection_exists("production-rag-v2"):
-
+if not clients.collection_exists("production-rag-v3"):
     clients.create_collection(
-        collection_name="production-rag-v2",
+        collection_name="production-rag-v3",
         vectors_config=VectorParams(
             size=384,
             distance=Distance.COSINE
         )
     )
 
-
-embedding_model = FastEmbedEmbeddings(
-    model_name="BAAI/bge-small-en-v1.5"
+embedding_model = HuggingFaceEndpointEmbeddings(
+    model="sentence-transformers/all-MiniLM-L6-v2",
+    huggingfacehub_api_token=os.getenv("HF_TOKEN")
 )
-
 
 vector_db = QdrantVectorStore(
     client=clients,
-    collection_name="production-rag-v2",
+    collection_name="production-rag-v3",
     embedding=embedding_model
 )
 
-
 retriever = vector_db.as_retriever(
-    search_kwargs={
-        "k": 3
-    }
+    search_kwargs={"k": 3}
 )
